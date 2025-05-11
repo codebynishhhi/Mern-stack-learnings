8edaabf373c5a7105c1beb5070e6aa0751742458
Gupta <nishigupta8@deloitte.com>
2025-05-11 22:25:49 +0530

    Mongodb connection established

---
+++ Bookify/server/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDbUrl = 'mongodb+srv://root:root@bookify-mern.hrvxsze.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookify-MERN'
---
+++ Bookify/server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { mongoDbUrl, PORT } from "./config.js";
+import mongoose from "mongoose";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// we use mongoose to connect to our mongoDB database
+mongoose
+  .connect(mongoDbUrl)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error, "error in connection");
+  });
